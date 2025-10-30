8f9cfdedbc3d26e478cebb65d34177daf971555b
unknown <njdairo13539@liceo.edu.ph>
2025-10-30 16:22:52 +0800

    feature:login page view

--- src/main/java/com/example/nextwear/LoginController.java
+++ src/main/java/com/example/nextwear/LoginController.java
@@ -3,7 +3,7 @@ package com.example.nextwear;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
