e639883ed0dd9f4636b5d6101571170410fc774c
unknown <njdairo13539@liceo.edu.ph>
2025-11-06 01:37:09 +0800

    refactor: adjust login layout

--- src/main/java/com/example/nextwear/LoginApplication.java
+++ src/main/java/com/example/nextwear/LoginApplication.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class LoginApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("NextWear Shop");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/com/example/nextwear/LoginController.java
+++ src/main/java/com/example/nextwear/LoginController.java
@@ -1,14 +1,20 @@
 package com.example.nextwear;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void onLogin() {
+        System.out.println("Email: " + emailField.getText());
+        System.out.println("Password: " + passwordField.getText());
     }
 }
